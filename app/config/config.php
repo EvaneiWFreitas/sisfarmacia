6adb18ec0a336af058074b1eabbbfda784bf59fd
EVANEI WILLIAM NASCIMENTO DE FREITAS <76791348+progwilliam@users.noreply.github.com>
2025-05-01 14:27:46 -0300

    Primeiro Commit - Criando a conexão com banco de dados do Sistema de Farmacia

---
+++ app/config/config.php
@@ -0,0 +1,20 @@
+<?php
+/*** CONEXÃO COM BANCO DE DADOS */
+
+/**VARIAVEIS */
+define('SERVIDOR','localhost');
+define('USUARIO','root');
+define('PASSWORD','');
+define('BD','sisfarmacia');
+
+$url = 'http://localhost/www.sisfarmacia.com';
+
+/**CONEXÃO */
+
+$servidor = "mysql:dbname=".BD.";host=".SERVIDOR;
+try {
+    $pdo = new PDO($servidor, USUARIO,PASSWORD,array(PDO::MYSQL_ATTR_INIT_COMMAND=>"SET NAMES utf8"));
+    echo "<script>alert('Banco de Dados conectado com Sucesso!!!')</script>";
+} catch (PDOException $e) {
+    echo "<script>alert('Erro ao tentar se conectar com a base de Dados!')</script>";
+}
